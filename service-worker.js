846213909890fa0e1cca67b3fae0189b8117be7d
djmaxqn2017-beep <djmaxqn2017@gmail.com>
2026-07-14 02:53:05 +0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,10 @@
+const CACHE_VERSION='taixiu-v7150';
 self.addEventListener('install',e=>self.skipWaiting());
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([
   self.clients.claim(),
   caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k))))
 ])));
 self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET')return;
   e.respondWith(fetch(e.request,{cache:'no-store'}).catch(()=>caches.match(e.request)));
 });
