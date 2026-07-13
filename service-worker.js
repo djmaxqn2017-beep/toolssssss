f4f64810bbcae58792465eb3b1b81328bdcd453d
djmaxqn2017-beep <djmaxqn2017@gmail.com>
2026-07-13 23:59:42 +0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,8 @@
 self.addEventListener('install',e=>self.skipWaiting());
-self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(k=>Promise.all(k.map(x=>caches.delete(x))))])));
-self.addEventListener('fetch',e=>e.respondWith(fetch(e.request,{cache:'no-store'}).catch(()=>caches.match(e.request))));
+self.addEventListener('activate',e=>e.waitUntil(Promise.all([
+  self.clients.claim(),
+  caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k))))
+])));
+self.addEventListener('fetch',e=>{
+  e.respondWith(fetch(e.request,{cache:'no-store'}).catch(()=>caches.match(e.request)));
+});
